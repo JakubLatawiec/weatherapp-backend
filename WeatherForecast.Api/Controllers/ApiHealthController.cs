fea28d9be9534e2fe84af63a49eec18b2f80571a
Jakub Latawiec <latawiec.jakub@outlook.com>
2025-04-12 12:52:53 +0200

    added api health

---
+++ WeatherForecast.Api/Controllers/ApiHealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiHealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetApiHealth()
+        {
+            return Ok("Api is working");
+        }
+    }
+}
